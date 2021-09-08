3b56c3938eb846360357f6b42f131280de45fde2
Nella <nella611@yandex.ru>
2021-09-08 13:31:14 +0200

    step7

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -11,21 +11,21 @@ def gcf(num1, num2):
             num2 = num2 % num1
     return str(num1 + num2)
 
-print(gcf(33,93))
-# def questions_answers():
-#     result = []
-#     for i in range(3):
-#         num1 = random.randint(1, 100)
-#         num2 = random.randint(1, 100)
-#         result.append((f'{num1} {num2}', gcf(num1, num2)))
-#     return result
-#
-#
-# def main():
-#     rule = 'Find the greatest common divisor of given numbers.'
-#     name = welcome_user()
-#     game(name, rule, questions_answers())
-#
-#
-# if __name__ == '__main__':
-#     main()
+
+def questions_answers():
+    result = []
+    for i in range(3):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        result.append((f'{num1} {num2}', gcf(num1, num2)))
+    return result
+
+
+def main():
+    rule = 'Find the greatest common divisor of given numbers.'
+    name = welcome_user()
+    game(name, rule, questions_answers())
+
+
+if __name__ == '__main__':
+    main()
